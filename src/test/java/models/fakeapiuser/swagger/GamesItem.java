1b4f692fd3b6a5818c4e3c5b0d56e490a33edb88
Ikonnikov Oleg <Ikonikov.oleg47@gmail.com>
2025-09-24 15:32:11 +0500

    correct

--- src/test/java/models/fakeapiuser/swagger/GamesItem.java
+++ src/test/java/models/fakeapiuser/swagger/GamesItem.java
@@ -1,7 +1,6 @@
 package models.fakeapiuser.swagger;
 
 import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
